3330d5da129d21294b39535e8d3d64e68f8d120a
Thomas Beaujean <thomas.beaujean@dayploy.fr>
2024-09-16 15:54:13 +0200

    Enable SwooleContextStorage if available

--- src/Server/Configurator/WithRequestHandler.php
+++ src/Server/Configurator/WithRequestHandler.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace K911\Swoole\Server\Configurator;
 
 use K911\Swoole\Server\RequestHandler\RequestHandlerInterface;
+use OpenTelemetry\Context\Context;
+use OpenTelemetry\Context\ContextStorage;
+use OpenTelemetry\Contrib\Context\Swoole\SwooleContextStorage;
 use Swoole\Http\Server;
 
 final class WithRequestHandler implements ConfiguratorInterface
@@ -21,6 +24,13 @@ final class WithRequestHandler implements ConfiguratorInterface
      */
     public function configure(Server $server): void
     {
+        // for open telemetry, the swooleContextStorage must be used
+        // if it is available, it is automatically enabled
+        if (class_exists(SwooleContextStorage::class)) {
+            // Use Swoole context storage
+            Context::setStorage(new SwooleContextStorage(new ContextStorage()));
+        }
+
         $server->on('request', [$this->requestHandler, 'handle']);
     }
 }
